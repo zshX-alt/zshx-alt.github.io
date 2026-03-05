c06d4abc8b6e56ecf4141f48712d647d12218fe1
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-05 10:19:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,15 +16,22 @@ def home():
 
 @app.route("/chat", methods=["POST"])
 def chat():
+    try:
 
-    data=request.json
-    message=data.get("message","")
+        data=request.json
+        message=data.get("message","")
 
-    response=model.generate_content(message)
+        response=model.generate_content(message)
 
-    return jsonify({
-        "reply":response.text
-    })
+        return jsonify({
+            "reply":response.text
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error":str(e)
+        }),500
+
 
 if __name__ == "__main__":
     port=int(os.environ.get("PORT",8080))
