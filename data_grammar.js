79e84f5db66b2e01eb41284c284c0d41a49f34bf
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-07 19:19:51 +0700

    Create data_grammar.js

---
+++ data_grammar.js
@@ -0,0 +1,16 @@
+const N4_GRAMMAR = {
+    "g01": { 
+        id: "g01", 
+        kanji: "～たい", 
+        baca: "V-masu + tai", 
+        arti: "Ingin... | Contoh: 寿司を食べたい", 
+        type: "grammar", state: "new", last: null 
+    },
+    "g02": { 
+        id: "g02", 
+        kanji: "～ながら", 
+        baca: "V-masu + nagara", 
+        arti: "Sambil... | Contoh: 歩きながら電話する", 
+        type: "grammar", state: "new", last: null 
+    }
+};
