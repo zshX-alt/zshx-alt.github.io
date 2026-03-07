8490906ae19b1b56989048abb8d9ed834d31c9a1
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-07 19:18:47 +0700

    Delete data_n4.js

--- data_n4.js
+++
@@ -1,7 +0,0 @@
-const N4_VOCAB = {
-    "v01": { id: "v01", kanji: "案内する", baca: "annai suru", arti: "memandu/mengantar", type: "vocab", state: "new", last: null },
-    "v02": { id: "v02", kanji: "準備する", baca: "junbi suru", arti: "bersiap-siap", type: "vocab", state: "new", last: null },
-    "v03": { id: "v03", kanji: "相談する", baca: "soudan suru", arti: "berkonsultasi", type: "vocab", state: "new", last: null },
-    "v04": { id: "v04", kanji: "連絡する", baca: "renraku suru", arti: "menghubungi", type: "vocab", state: "new", last: null },
-    "v05": { id: "v05", kanji: "注意する", baca: "chuui suru", arti: "berhati-hati", type: "vocab", state: "new", last: null }
-};
