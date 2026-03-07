9a892c27ce1847623d9b2825e80b5ad2308c87cc
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-07 19:14:06 +0700

    Update data_n4.js

--- data_n4.js
+++ data_n4.js
@@ -1,15 +1,7 @@
-const N4_DATASET = {
-    // --- VOCABULARY SECTION ---
-    "v01": { id: "v01", kanji: "通う", baca: "kayou", arti: "pulang pergi", type: "vocab", tags: ["harian"], s: 1, d: 5, last: null, state: 'new' },
-    "v02": { id: "v02", kanji: "乗り換える", baca: "norikaeru", arti: "pindah kendaraan", type: "vocab", tags: ["transportasi"], s: 1, d: 5, last: null, state: 'new' },
-    "v03": { id: "v03", kanji: "資料", baca: "shiryou", arti: "dokumen", type: "vocab", tags: ["kantor"], s: 1, d: 5, last: null, state: 'new' },
-    "v04": { id: "v04", kanji: "予定", baca: "yotei", arti: "rencana", type: "vocab", tags: ["waktu"], s: 1, d: 5, last: null, state: 'new' },
-    "v05": { id: "v05", kanji: "調べる", baca: "shiraberu", arti: "memeriksa", type: "vocab", tags: ["belajar"], s: 1, d: 5, last: null, state: 'new' },
-
-    // --- GRAMMAR SECTION ---
-    "g01": { id: "g01", kanji: "～方がいい", baca: "hou ga ii", arti: "lebih baik...", type: "grammar", tags: ["saran"], s: 1, d: 5, last: null, state: 'new' },
-    "g02": { id: "g02", kanji: "～なければならない", baca: "nakereba naranai", arti: "harus...", type: "grammar", tags: ["keharusan"], s: 1, d: 5, last: null, state: 'new' },
-    "g03": { id: "g03", kanji: "～てもいい", baca: "temo ii", arti: "boleh...", type: "grammar", tags: ["izin"], s: 1, d: 5, last: null, state: 'new' },
-    "g04": { id: "g04", kanji: "～ことができる", baca: "koto ga dekiru", arti: "bisa/dapat...", type: "grammar", tags: ["kemampuan"], s: 1, d: 5, last: null, state: 'new' },
-    "g05": { id: "g05", kanji: "～すぎる", baca: "sugiru", arti: "terlalu...", type: "grammar", tags: ["kondisi"], s: 1, d: 5, last: null, state: 'new' }
+const N4_VOCAB = {
+    "v01": { id: "v01", kanji: "案内する", baca: "annai suru", arti: "memandu/mengantar", type: "vocab", state: "new", last: null },
+    "v02": { id: "v02", kanji: "準備する", baca: "junbi suru", arti: "bersiap-siap", type: "vocab", state: "new", last: null },
+    "v03": { id: "v03", kanji: "相談する", baca: "soudan suru", arti: "berkonsultasi", type: "vocab", state: "new", last: null },
+    "v04": { id: "v04", kanji: "連絡する", baca: "renraku suru", arti: "menghubungi", type: "vocab", state: "new", last: null },
+    "v05": { id: "v05", kanji: "注意する", baca: "chuui suru", arti: "berhati-hati", type: "vocab", state: "new", last: null }
 };
