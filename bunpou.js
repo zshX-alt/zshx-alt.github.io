cee5abc99a6332d9addfc6c634dcf40329642e47
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-07 18:11:59 +0700

    Delete bunpou.js

--- bunpou.js
+++
@@ -1,7 +0,0 @@
-// bunpo.js
-const n4Bunpo = [
-    { id: 101, pola: "~てから", arti: "Setelah...", status: "new" },
-    { id: 102, pola: "~ことができる", arti: "Dapat/Bisa...", status: "new" },
-    { id: 103, pola: "~なければならない", arti: "Harus...", status: "new" },
-    // Tambahkan terus sampai 80 pola
-];
