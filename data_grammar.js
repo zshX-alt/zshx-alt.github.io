e25fc2d55e84097382a20bf9c8f3541da8fd5eef
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-07 18:55:01 +0700

    Create data_grammar.js

---
+++ data_grammar.js
@@ -0,0 +1,9 @@
+const N4_GRAMMAR = {
+    "g01": { 
+        id: "g01", 
+        kanji: "～たい", 
+        baca: "V-masu + tai", 
+        arti: "Ingin... | Contoh: 寿司を食べたい (Ingin makan sushi)", 
+        type: "grammar", state: 'new' 
+    }
+};
