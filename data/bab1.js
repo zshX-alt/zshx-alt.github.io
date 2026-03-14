a48845f2d5b6f8cfb3867169f58a8cf5710e174c
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-15 01:16:26 +0700

    Create data/bab1.js

---
+++ data/bab1.js
@@ -0,0 +1,19 @@
+const bab1 = {
+    title: "第１課 (Bab 1)",
+    description: "Perkenalan diri, kewarganegaraan, dan pekerjaan.",
+    bunpou: [
+        {
+            pola: "～は ～です",
+            penjelasan: "Pola dasar untuk menyatakan identitas (A adalah B).",
+            jp: "<ruby>私<rt>わたし</rt></ruby> は 学生 です。",
+            id: "Saya adalah mahasiswa."
+        },
+        {
+            pola: "～は ～じゃありません",
+            penjelasan: "Bentuk negatif dari 'desu' (Bukan/Tidak).",
+            jp: "サントスさん は 医者 じゃありません。",
+            id: "Sdr. Santos bukan dokter."
+        }
+    ]
+};
+export default bab1;
