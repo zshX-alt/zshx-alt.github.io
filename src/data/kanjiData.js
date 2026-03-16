7a9e0e5449845ab0e15c0369ef6cfb9516a65944
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-16 15:54:26 +0700

    Create src/data/kanjiData.js

---
+++ src/data/kanjiData.js
@@ -0,0 +1,12 @@
+// src/data/kanjiData.js
+export const kanjiMaster = {
+    "bab1": [
+        { char: "私", on: "shi", kun: "watashi", arti: "Saya" },
+        { char: "学", on: "gaku", kun: "manabu", arti: "Belajar" }
+    ],
+    "bab2": [
+        { char: "日", on: "nichi", kun: "hi", arti: "Hari/Matahari" },
+        { char: "本", on: "hon", kun: "moto", arti: "Asal/Buku" }
+    ]
+    // ... dst
+};
